27cf7288c59ecd64dcc50c1944099381069f0bb3
horica78 <90985189+horica78@users.noreply.github.com>
2021-09-18 22:41:56 +0300

    Update class_xml_test.php

--- class_xml_test.php
+++ class_xml_test.php
@@ -49,43 +49,7 @@ class ResponseXML extends DOMDocument {
 		$this->add_dets($array, $this);
 	}
 }
-function array_to_xml(array $arr, SimpleXMLElement $xml) {
-	foreach ($arr as $k => $v) {
 
-		$attrArr = array();
-		$kArray = explode(' ',$k);
-		$tag = array_shift($kArray);
-
-		if (count($kArray) > 0) {
-			foreach($kArray as $attrValue) {
-				$attrArr[] = explode('=',$attrValue);                   
-			}
-		}
-
-		if (is_array($v)) {
-			if (is_numeric($k)) {
-				array_to_xml($v, $xml);
-			} else {
-				$child = $xml->addChild($tag);
-				if (isset($attrArr)) {
-					foreach($attrArr as $attrArrV) {
-						$child->addAttribute($attrArrV[0],$attrArrV[1]);
-					}
-				}                   
-				array_to_xml($v, $child);
-			}
-		} else {
-			$child = $xml->addChild($tag, $v);
-			if (isset($attrArr)) {
-				foreach($attrArr as $attrArrV) {
-					$child->addAttribute($attrArrV[0],$attrArrV[1]);
-				}
-			}
-		}               
-	}
-
-	return $xml;
-}
 //header('Content-Type: text/xml');
 $a['root']['hotels'][0]['hotel']['name'] = 'test';
 $a['root']['hotels'][0]['description']['cdata'] = 'test';
@@ -102,63 +66,4 @@ $oResponseXml->createXML($a);
 //header('Content-Type: text/xml');
 header('Content-Type: text/plain');
 echo $oResponseXml->saveXML();
-
-
-$xml = array_to_xml($a, new SimpleXMLElement('<root/>'))->asXML();
-
-echo "$xml\n";
-$xml_parser = new DOMDocument();
-$xml_parser->preserveWhiteSpace = false;
-$xml_parser->formatOutput=true;
-$xml_parser->loadXML($xml);
-echo $xml_parser->saveXML();
-
-function array_to_xml2($array, $root, $element) {
-    $xml = new SimpleXMLElement("<{$root}/>");
-    foreach ($array as $value) {
-        $elem = $xml->addChild($element);
-        xml_recurse_child($elem, $value);
-    }
-    return $xml;
-}
-
-function xml_recurse_child(&$node, $child) {
-    foreach ($child as $key=>$value) {
-        if(is_array($value)) {
-            foreach ($value as $k => $v) {
-                if(is_numeric($k)){
-                    xml_recurse_child($node, array($key => $v));
-                }
-                else {
-                    $subnode = $node->addChild($key);
-                    xml_recurse_child($subnode, $value);
-                }
-            }
-        }
-        else {
-            $node->addChild($key, $value);
-        }
-    }   
-} 
-
-echo array_to_xml2($a, $root='root', $element='child');
-function array2Xml($data, $xml = null)
-{
-    if (is_null($xml)) {
-        $xml = simplexml_load_string('<' . key($data) . '/>');
-        $data = current($data);
-        $return = true;
-    }
-    if (is_array($data)) {
-        foreach ($data as $name => $value) {
-            array2Xml($value, is_numeric($name) ? $xml : $xml->addChild($name));
-        }
-    } else {
-        $xml->{0} = $data;
-    }
-    if (!empty($return)) {
-        return $xml->asXML();
-    }
-}
-echo array2Xml($a);
 ?>
